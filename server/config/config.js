9a3c17d370ba8d64e34f040a71dad8f4cde6159f
Max <cornejo.maximino@gmail.com>
2020-02-24 21:36:35 -0600

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://maxico:jkgNJqsHR51WLLVU@cluster0-1jjdp.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
